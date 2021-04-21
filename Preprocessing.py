578cff97879688f43e9c7e494956d0fc1f7d3f5e
Taha Abdalzez <57445405+0xtaha@users.noreply.github.com>
2021-04-21 02:09:08 +0200

    add EEGAN files , update Preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,4 @@
 import os
-import re
 from skimage.transform import resize
 from matplotlib import pyplot
 import numpy as np
@@ -7,33 +6,41 @@ import cv2
 from tqdm.notebook import tqdm, trange
 
 
-def pixalate_image(image, scale_percent = 40 , downsampling_mode = cv2.INTER_AREA):
+def pixalate_image(image, resize_dim = (96 , 96) , downsampling_mode = cv2.INTER_AREA , same_size = True):
 
     w,h,_ = image.shape
-    width = int(w * scale_percent / 100)
-    height = int(h * scale_percent / 100)
-    dim = (width, height)
-    small_image = cv2.resize(image, dim, interpolation = downsampling_mode )
+    
+    if (downsampling_mode == None ):
+        small_image = cv2.resize(image, resize_dim )
+    else:
+        small_image = cv2.resize(image, resize_dim, interpolation = downsampling_mode )
     
     # scale back to original size
-    dim = (w,h)
-    low_res_image = cv2.resize(small_image, dim, interpolation =  downsampling_mode )
+    if (same_size == False) :
+        return small_image
+
+    resize_dim = (w,h)
+    if (downsampling_mode == None ):
+        low_res_image = cv2.resize(small_image, resize_dim )
+    else:
+        low_res_image = cv2.resize(small_image, resize_dim, interpolation = downsampling_mode )
+
     return low_res_image
 
 ## Progress bar is to be added
-def Data_Preprocessing(images_list ,path, Preprocessed_Data_Path , pixelation_scale = 40 ,DownSamplingMode = cv2.INTER_AREA  ):
+def Data_Preprocessing(images_list ,path, Preprocessed_Data_Path , resize_dim = (96 , 96) ,DownSamplingMode = cv2.INTER_AREA  ):
     progress = tqdm(total= len(images_list), position=0)
-    for filepath in images_list:
-        filename , ext = filepath.split('/')[-1].split('.')
+    for i , filepath in enumerate(images_list):
         image = pyplot.imread(filepath) # read the image file and save into an array
         if len(image.shape) > 2:
           # Resize the image so that every image is the same size
           HighRes = resize(image, (256, 256))
           # Add this image to the high res dataset
           # Rescale it 0.5x and 2x so that it is a low res image but still has 256x256 resolution
-          LowRes = pixalate_image(HighRes , scale_percent = pixelation_scale , downsampling_mode = DownSamplingMode)
-          np.save(os.path.join(Preprocessed_Data_Path, path+'_y', filename + '.npy'), HighRes)
-          np.save(os.path.join(Preprocessed_Data_Path, path+'_x',filename + '.npy'), LowRes)
+          LowRes = pixalate_image(HighRes , resize_dim , downsampling_mode = DownSamplingMode)
+          name = "{}".format("{0:05d}".format(i))
+          np.save(os.path.join(Preprocessed_Data_Path, path+'_y', name + '.npy'), HighRes)
+          np.save(os.path.join(Preprocessed_Data_Path, path+'_x',name + '.npy'), LowRes)
           os.remove(filepath)
           progress.update(1)
     print('Done ... ')                          
