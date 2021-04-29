277602fa8d730b9ad24be844c593fd4d01ed71f8
Taha Abdalzez <57445405+0xtaha@users.noreply.github.com>
2021-04-29 14:22:51 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,7 +1,18 @@
-from tensorflow.image import psnr, ssim
+def Psnr(im1 , im2 , max_value = 1.0):
+  im1 = tf.image.convert_image_dtype(im1, tf.float32)
+  im2 = tf.image.convert_image_dtype(im2, tf.float32)
+  psnr = tf.image.psnr(im1, im2, max_val= max_value)
+  return float(psnr)
 
-def ssim(y_pred, y_true):
-  return ssim(y_true, y_pred , max_val=2.0)
+def mse(target, ref):
+    # the MSE between the two images is the sum of the squared difference between the two images
+    err = np.sum((target.astype('float') - ref.astype('float')) ** 2)
+    err /= float(target.shape[0] * target.shape[1])
+    
+    return err
 
-def psnr(y_pred, y_true):
-  return psnr(y_true, y_pred , max_val=1.0)
+def SSIM(im1 , im2 , max_value = 2.0):
+  im1 = tf.image.convert_image_dtype(im1, tf.float32)
+  im2 = tf.image.convert_image_dtype(im2, tf.float32)
+  SSIM = tf.image.ssim(im1, im2, max_val= max_value)
+  return float(SSIM)
